dab2fa6b4331c550a8f3892f4a8665e31ffedc4c
Alfredo Paz <alfredopaz7@gmail.com>
2020-06-09 13:01:47 -0500

    actorsName.py cgi testign like a cgi and working

--- cgi-bin/actorsName.py
+++ cgi-bin/actorsName.py
@@ -5,12 +5,12 @@ import sqlite3
 
 form = cgi.FieldStorage() 
 name = form.getvalue('name')
-name = 'Ford' #only for testing
+#name = 'Ford' #only for testing
 
 conn = sqlite3.connect('imdb.db')
 cur = conn.cursor()
 sql = "SELECT * FROM Actor WHERE Name like '%" + name + "%'"
-print(sql)
+#print(sql)
 data = []
 for row in cur.execute(sql):
   data.append({
@@ -18,5 +18,6 @@ for row in cur.execute(sql):
     'name': row[1]
     })
 conn.close()
+print("Content-Type: application/json\n\n")
 print(json.dumps(data))
 
