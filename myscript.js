9e048cdb7d35651ab3126f1766da93e5f13083ee
Alfredo Paz <alfredopaz7@gmail.com>
2020-06-09 12:05:55 -0500

    myscript.js untested skell version

---
+++ myscript.js
@@ -0,0 +1,18 @@
+function query1(name){
+  var url = 'cgi-bin/actorsName.py?name='+name;
+  console.log(ur);
+  ajax(url, render)
+}
+function render(data){
+  console.log(data)
+}
+function ajax(url, callback){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(this.responseText);
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
