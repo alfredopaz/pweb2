42b158531290d47a8f853da322c0583c23abcfa7
Alfredo Paz <alfredopaz7@gmail.com>
2020-06-09 13:58:51 -0500

    Bug about responseText to json solved, and rendering the html table

--- myscript.js
+++ myscript.js
@@ -5,12 +5,36 @@ function query1(name){
 }
 function render(data){
   console.log(data)
+  //TODO: how about empty data?
+  var table = "<table id='answere1'>";
+  var row = data[0];
+  table += '<tr>';
+  for(key in row){
+    table += '<th>';
+    table += key;
+    table += '</th>';
+  }
+  table += '</tr>';
+  for(var i in data){
+    table += '<tr>';
+    row = data[i];
+    for(key in row){
+      table += '<td>';
+      table += row[key];
+      table += '</td>';
+    }
+    table += '</tr>';
+  }
+  table += "</table>";
+  console.log(table)
+  document.getElementById("answere1").innerHTML = table;
 }
 function ajax(url, callback){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      callback(this.responseText);
+      json = JSON.parse(this.responseText);
+      callback(json);
     }
   };
   xhttp.open("GET", url, true);
